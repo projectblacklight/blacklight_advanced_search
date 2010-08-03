b8175245ba25524e32d97791fbe8310fade1b19f
Jonathan Rochkind <jonathan@dnil.net>
2010-08-03 19:21:39 -0400

    template does not need to add 'require' statement to local ApplicationHelper, not neccesary.

--- template.rb
+++ template.rb
@@ -18,19 +18,6 @@ else
   puts "** There appears to be an existing blacklight_advanced_search configuration file.  Skipping....."
 end
 
-# require for application_helper
-puts "\n* Modifying your app/helpers/application_helper.rb file..."
-old_app_helper_dep = "require 'vendor/plugins/blacklight/app/helpers/application_helper.rb'"
-new_app_helper_dep = "require 'vendor/plugins/blacklight_advanced_search/app/helpers/application_helper.rb'"
-app_helper = File.read('app/helpers/application_helper.rb')
-
-if app_helper.scan(new_app_helper_dep).empty? and !app_helper.scan(old_app_helper_dep).empty?
-  app_helper = app_helper.gsub(old_app_helper_dep,"#{old_app_helper_dep}\n#{new_app_helper_dep}")
-  File.open('app/helpers/application_helper.rb', 'w'){|f| f.puts app_helper}
-else
-  puts "** It appears that your application helper is already requiring the blacklight_advanced_search application helper. Skipping....."
-end
-
 if yes?("Would you like to install the gem dependecies now?")
   if yes? "Do you want to install gems using sudo?"
     user = run("whoami").chomp
