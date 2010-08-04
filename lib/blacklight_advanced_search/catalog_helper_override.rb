46c143281c4de877d65240cc6efc5afcd066ac53
Jonathan Rochkind <jonathan@dnil.net>
2010-08-04 18:05:10 -0400

    new approach to displaying adv facet limits in sidebar, that distinguishes the advanced 'inclusive or' selections from ordinary 'and' selections.

--- lib/blacklight_advanced_search/catalog_helper_override.rb
+++ lib/blacklight_advanced_search/catalog_helper_override.rb
@@ -1,32 +1,5 @@
 module BlacklightAdvancedSearch::CatalogHelperOverride
 
-  def facet_in_params?(field, value)
-    (params[:f] and params[:f][field] and params[:f][field].include?(value)) or (params[:f_inclusive] and params[:f_inclusive][field] and params[:f_inclusive][field].keys.include?(value))
-  end
-
-  
-  # copies the current params (or whatever is passed in as the 3rd arg)
-  # removes the field value from params[:f] and params[:fq]
-  # removes the field if there are no more values in params[:f][field]
-  # completely removes the params[:fq] because of OR logic
-  # removes additional params (page, id, etc..)
-  def remove_facet_params(field, value, source_params=params)
-    p = source_params.dup.symbolize_keys!
-    # need to dup the facet values too,
-    # if the values aren't dup'd, then the values
-    # from the session will get remove in the show view...
-    p[:f] = p[:f].dup.symbolize_keys! if p[:f]
-    p[:f_inclusive] = p[:f_inclusive].dup.symbolize_keys! if p[:fq]
-    p.delete :page
-    p.delete :id
-    p.delete :counter
-    p.delete :commit
-    p[:f_inclusive][field] = p[:f_inclusive][field].delete_if{|k,v| k == value} if p[:f_inclusive] and p[:f_inclusive][field] and p[:f_inclusive][field][value]
-    p[:f_inclusive].delete(field) if p[:f_inclusive] and p[:f_inclusive][field] and  p[:f_inclusive][field].size == 0
-    p[:f][field] = p[:f][field] - [value] if p[:f] and p[:f][field]
-    p[:f].delete(field) if p[:f][field].size == 0 if p[:f] and p[:f][field]
-    p
-  end
   
   
   def remove_advanced_keyword_query(field, my_params = params)
@@ -43,4 +16,37 @@ module BlacklightAdvancedSearch::CatalogHelperOverride
     end
     my_params
   end
+
+  # Special display for facet limits that include adv search inclusive
+  # or limits.
+  def render_facet_limit(solr_field)
+    unless (@advanced_query && @advanced_query.filters.keys.include?( solr_field))
+      super(solr_field)
+    else
+      # our own display
+      render(:partial => "blacklight_advanced_search/facet_limit", :locals=> {:solr_field => solr_field})
+    end
+  end
+
+  def remove_advanced_facet_param(field, value, my_params = params)
+    my_params = my_params.dup
+    if (my_params[:f_inclusive] && 
+        my_params[:f_inclusive][field] &&
+        my_params[:f_inclusive][field].include?(value))
+        
+      my_params[:f_inclusive][field] = my_params[:f_inclusive][field].dup
+      my_params[:f_inclusive][field].delete(value)
+      
+      my_params[:f_inclusive].delete(field) if my_params[:f_inclusive][field].length == 0
+      
+      my_params.delete(:f_inclusive) if my_params[:f_inclusive].length == 0
+    end
+
+    my_params.delete_if do |key, value| 
+      [:page, :id, :counter, :commit].include?(key)
+    end
+    
+    my_params
+  end
+  
 end
