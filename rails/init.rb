b33d9f07f5ef6867863bfd014913b1750c48094f
Jonathan Rochkind <jonathan@dnil.net>
2010-08-04 10:59:59 -0400

    use some clever Rails tricks to try and make our include's and helper additions work regardless of cache_classes setting.

--- rails/init.rb
+++ rails/init.rb
@@ -11,11 +11,47 @@ config.after_initialize do
   
   Blacklight.config[:search_fields] << {:display_label => 'Advanced', :key => BlacklightAdvancedSearch.config[:search_field], :include_in_simple_select => false} if defined? :Blacklight
 
-  CatalogController.send(:include, BlacklightAdvancedSearch::ControllerOverride  )
-  CatalogController.helper( BlacklightAdvancedSearch::RenderConstraintsOverride )
-  CatalogController.helper( BlacklightAdvancedSearch::CatalogHelperOverride )
+
+  # Do these things in a to_prepare block, to try and make them work
+  # in development mode with class-reloading. The trick is we can't
+  # be sure if the controllers we're modifying are being reloaded in
+  # dev mode, if they are in the BL plugin and haven't been copied to
+  # local, they won't be. But we do our best. 
+  config.to_prepare do
+    # Ordinary module over-ride to CatalogController
+    CatalogController.send(:include,  
+        BlacklightAdvancedSearch::ControllerOverride  
+    ) unless
+      CatalogController.include?(   
+        BlacklightAdvancedSearch::ControllerOverride 
+      )
+
+    # Add helpers to CatalogController and SearchHistoryController. Use
+    # some tricks to only add our helper once, may need to be done differently
+    # in Rails3. 
+    CatalogController.add_template_helper(
+      BlacklightAdvancedSearch::RenderConstraintsOverride 
+    ) unless 
+        CatalogController.master_helper_module.include?(
+          BlacklightAdvancedSearch::RenderConstraintsOverride
+        )
+        
+    CatalogController.add_template_helper(
+      BlacklightAdvancedSearch::CatalogHelperOverride 
+    ) unless
+      CatalogController.master_helper_module.include?( 
+        BlacklightAdvancedSearch::CatalogHelperOverride 
+      )
+      
   
-  SearchHistoryController.helper( BlacklightAdvancedSearch::RenderConstraintsOverride )
+    SearchHistoryController.add_template_helper(
+      BlacklightAdvancedSearch::RenderConstraintsOverride 
+    ) unless
+      SearchHistoryController.master_helper_module.include?( 
+        BlacklightAdvancedSearch::RenderConstraintsOverride 
+      )
+
+  end
 end
 
 
