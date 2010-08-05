e14e3c686e0caf71c7482082e3c721d26bcccba8
Jonathan Rochkind <jonathan@dnil.net>
2010-08-05 16:59:24 -0400

    slightly clearer instructions

--- template.rb
+++ template.rb
@@ -1,17 +1,15 @@
 
 plugin_root = "vendor/plugins/blacklight_advanced_search"
 
-if yes?("install example Blacklight Advanced Search config file?")
+if yes?("Install optional example Blacklight Advanced Search config file?")
 
     
   advanced_search_config = "config/initializers/blacklight_advanced_search_config.rb"
   
   if (!File.exists? advanced_search_config) || yes?("Over-write existing config/initializers/blacklight_advanced_search_config.rb ?")
-    puts "\n* Copying sample Blacklight Advanced Search config to your config/initializers directory..."
-
-    
-    
+        
     FileUtils.cp( File.join(plugin_root, advanced_search_config), advanced_search_config )
+    puts "\n* Copied example Blacklight Advanced Search config to config/initializers/blacklight_advanced_search.rb"
   end
 
 end
@@ -19,3 +17,5 @@ end
 if yes?("Install all application gem dependencies using 'sudo', including new ones from this plugin?")
   rake "gems:install", :sudo=>true
 end
+
+
