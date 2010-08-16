cab56e0fa27c192bd875d17bb67201b64e32b7a1
Jonathan Rochkind <jonathan@dnil.net>
2010-08-16 16:26:01 -0400

    DRY up checkbox feedback JS, use class selectors instead of IDs.

--- assets/javascripts/blacklight_advanced_search_javascript.js
+++ assets/javascripts/blacklight_advanced_search_javascript.js
@@ -16,58 +16,50 @@ $(document).ready(function() {
   
   /* Stuff for handling the checkboxes */
   /* When you click a checkbox, update readout */
-  $("#advanced_search_facets input").each(function(){
-    if ($(this).attr('type') == 'checkbox' & $(this).is(':checked')) {
-    var selected_facets_element = $(this).parent().parent().parent().children('h3').children('span');
-    if (selected_facets_element.children('span').text() == "") {
-      selected_facets_element.children('span').text($(this).next('label').text());
-    }else{
-      selected_facets_element.children('span').text(selected_facets_element.children('span').text() + " OR " + $(this).next('label').text());
+  
+  
+  /* Pass in a jquery obj holding the "selected facet element" spans,  get back
+     a string for display representing currently checked things. */
+  function checkboxesToString(checkbox_elements) {
+    var selectedLabels = new Array();
+    checkbox_elements.each(function() {
+        if ($(this).is(":checked")) {
+          selectedLabels.push( $(this).next('label').text());
+        }
+    });
+    return selectedLabels.join(" OR ");
+  }
+  
+  //Pass in JQuery object of zero or more <div class="facet_item"> blocks,
+  //that contain an h3, some checkboxes, and a span.adv_facet_selections for
+  //display of current selections. Will update the span. 
+  function updateSelectedDisplay(facet_item) {
+    var checkboxes = $(facet_item).find("input:checkbox");
+    var displaySpan = $(facet_item).find("span.adv_facet_selections");
+    var displayStr = checkboxesToString( checkboxes );
+    
+    displaySpan.text( displayStr );
+    if (displayStr == "") {
+      displaySpan.hide();
+    } else {
+      displaySpan.show();
     }
-    if (selected_facets_element.attr("style") == "display: none;"){
-      selected_facets_element.attr("style","")
-    }
-    }
+    
+    
+  }
+  
+  // on page load, set initial properly
+  $(".facet_item").each(function() {
+      updateSelectedDisplay(this);
+  });
+
+  //change on checkbox change
+  $(".facet_item input:checkbox").change( function() {
+      updateSelectedDisplay( $(this).closest(".facet_item"));
   });
   
   
-  /* On page load, make readout match initial load. */
-    $("#advanced_search_facets input").each(function(){
-      if($(this).attr('type') == 'checkbox'){
-      $(this).click(function(){
-      var selected_facets_element = $(this).parent().parent().parent().children('h3').children('span');
-        var current_text = selected_facets_element.children('span').text();
-      var new_text = $(this).next('label').text();
-        if($(this).is(':checked')){
-        if(current_text == ''){
-          swap_text = new_text;
-        }else{
-          swap_text = current_text + " OR " + new_text;
-        }
-      }else{
-        if(current_text.replace("(","_").replace(")","_").search(new_text.replace("(","_").replace(")","_") + " OR ") > -1  ){
-          swap_text = current_text.replace(new_text + " OR ", '');
-        }else if(current_text.replace("(","_").replace(")","_").search(" OR " + new_text.replace("(","_").replace(")","_")) > -1  ){
-          swap_text = current_text.replace(" OR " + new_text, '');
-        }else{
-          swap_text = current_text.replace(new_text, '');
-        }
-      }
-      selected_facets_element.children('span').text(swap_text);
-      if(selected_facets_element.children('span').text() == " OR "){
-        selected_facets_element.children('span').text() == "";
-      }else if(selected_facets_element.children('span').text().substr(-4) == " OR "){
-        selected_facets_element.children('span').text(selected_facets_element.children('span').text().substr(0,selected_facets_element.children('span').text().length-4))
-      }
-      if(selected_facets_element.children('span').text() == ""){
-        selected_facets_element.attr("style", "display:none;");
-      }else{
-        selected_facets_element.attr("style", "display:inline;");
-      }
-      });
-    }
-   
-  });
+  /
 
   
 });
