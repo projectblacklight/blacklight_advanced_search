7b56f95606fabdafec770c8e5b1e51816dea074b
Jonathan Rochkind <jonathan@dnil.net>
2010-08-17 11:24:24 -0400

    For search form within an existing search context, display facets as fetched/calculated within that context.
    This ended up being a bit tricky. We need to execute our solr facet fetch with all parameters appropriate for the current search context, _including_ those added by other plugins. To do that, AdvancedController now sub-classes CatalogController,
    so CatalogController#solr_search_params can be called, since that's the one that has been plugin-enhanced. The params
    returned by solr_search_params then have all facet-related params removed, along with other params we're not interested in.
    
    Not entirely sure how this will effect things if you use a different request handler for advanced; probably depends on
    the individual plugin, should work fine with the 'officialish' ones we have already.
    
    Additionally, AdvancedController now 'include's AdvancedHelper, to get access to the #advanced_search_context method,
    to see if there is an existing search context that needs to be taken account of in facet fetch.

--- app/controllers/advanced_controller.rb
+++ app/controllers/advanced_controller.rb
@@ -1,4 +1,8 @@
-class AdvancedController < ApplicationController
+# Need to sub-class CatalogController so we get all other plugins behavior
+# for our own "inside a search context" lookup of facets. 
+class AdvancedController < CatalogController
+  include AdvancedHelper # so we get the #advanced_search_context method
+  
   before_filter :setup_advanced_search_css, :setup_advanced_search_js, :only => :index
   
   def index
@@ -9,10 +13,29 @@ class AdvancedController < ApplicationController
 
   protected
   def get_advanced_search_facets
-    input = {
+    
+    search_context_params = {}    
+    if (advanced_search_context.length > 0 )
+      # We have a search context, need to fetch facets from within
+      # that context.
+      search_context_params = solr_search_params
+      # But need to delete any facet-related params, or anything else
+      # we want to set ourselves or inherit from Solr request handler
+      # defaults. 
+      search_context_params.delete_if do |k, v|
+        k = k.to_s
+        (["facet.limit", "facet.sort", "f", "facets", "facet.fields", "qt", "per_page"].include?(k) ||                
+          k =~ /f\..+\.facet\.limit/ ||
+          k =~ /f\..+\.facet\.sort/
+        )        
+      end
+    end
+    
+    input = search_context_params.merge(
       :qt=>BlacklightAdvancedSearch.config[:qt],
       :per_page=>0
-    }.merge( BlacklightAdvancedSearch.config[:form_solr_parameters] )
+    ).merge( BlacklightAdvancedSearch.config[:form_solr_parameters] )
+    
     Blacklight.solr.find(input)
   end
   def setup_advanced_search_css
