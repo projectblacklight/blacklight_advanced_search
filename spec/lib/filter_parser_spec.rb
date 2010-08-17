e9cd6ce693e3e4622997da42b32516d06504bc4b
Jonathan Rochkind <jonathan@dnil.net>
2010-08-17 16:57:02 -0400

    filter parser spec passes now again

--- spec/lib/filter_parser_spec.rb
+++ spec/lib/filter_parser_spec.rb
@@ -1,13 +1,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+def setFilters(f)
+  @filters = f
+end
+
+def filters
+  @filters
+end
+
+
 describe "BlacklightAdvancedSearch::FilterParser" do
   include BlacklightAdvancedSearch::FilterParser
+  
   describe "filter processing" do
     it "should generate an appropriate fq param" do
-      process_filters({:fq => {"location" => {"Online" => 1, "Library" => 1}, "format" => {"Book" => 1, "Thesis" => 1}}}).should match(/format\:\((\"Book\"|\"Thesis\") OR (\"Thesis\"|\"Book\")\), location\:\((\"Library\"|\"Online\") OR (\"Online\"|\"Library\")\)/)
-    end
-    it "should AND facets that are selected from the Refine section (those with a value of 2)" do
-      process_filters({:fq => {"location" => {"Online" => "1", "Library" => "1", "My House" => "2"}}}).should match(/location\:\((\"Online\"|\"Library\") OR (\"Library\"|\"Online\") AND \"My House\"\)/)
-    end
+      setFilters(:format => ["Book", "Thesis"], :location=>["Online", "Library"])
+
+      fq_params = generate_solr_fq
+
+      fq_params.find {|a| a =~ /format\:\((\"Book\"|\"Thesis\") +OR +(\"Thesis\"|\"Book\")/}.should_not be_nil
+
+      fq_params.find {|a| a =~ /location\:\((\"Library\"|\"Online\") +OR +(\"Library\"|\"Online\")/}.should_not be_nil
+      
+
+    end    
   end
 end
