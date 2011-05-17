4e0d9e1021493da6c19eac6de0d635b2238b634d
Jonathan Rochkind <jonathan@dnil.net>
2011-05-17 18:42:17 -0400

    don't set :solr_type in the example config
    that encourages a bad habit, better to leave it unset using defualt. Also added more comments discouraging bad habits.

--- config/initializers/blacklight_advanced_search_config.rb
+++ config/initializers/blacklight_advanced_search_config.rb
@@ -3,14 +3,15 @@
 # called "advanced" in the example Blacklight solrconfig.xml:
 # http://github.com/projectblacklight/blacklight-jetty/blob/master/solr/conf/solrconfig.xml
 #
-# Using a seperate request handler is just one option, in many cases it's
-# simpler to use your default solr request handler set in Blacklight itself,
-# and you may not need any of this configuration. See README. 
+# NOTE WELL: Using a seperate request handler is just one option, in most cases
+# it's simpler to use your default solr request handler set in Blacklight itself,
+# in which case you would not want to set any of the configuration below!
+# See README. 
 
 BlacklightAdvancedSearch.config.merge!(
   # This will be used later when edismax is returning the expected results
   #:solr_type => "edismax",
-  :solr_type => "dismax",
+  #:solr_type => "dismax", # this is default, so no need to specify it. 
   # :search_field => "advanced", # name of key in Blacklight URL, no reason to change usually. 
   :qt => "advanced" # name of Solr request handler, leave unset to use the same one as your Blacklight.config[:default_qt]  
 )
