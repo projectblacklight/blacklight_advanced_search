3704cf1f973116f82d70ff49a2d2391fde2b617c
Jonathan Rochkind <jonathan@dnil.net>
2011-06-09 16:05:24 -0400

    send include to CatalogController, not Blacklight::Controller module
    for reasons not entirely clear, trying to enhance the module in a
    to_prepare doesn't work/isn't reliable. could debug it, but it's
    confusing. CatalogController specifically works, and that's how
    it worked in bl2 anyhow.

--- lib/blacklight_advanced_search/engine.rb
+++ lib/blacklight_advanced_search/engine.rb
@@ -18,13 +18,13 @@ module BlacklightAdvancedSearch
     # in development mode with class-reloading. The trick is we can't
     # be sure if the controllers we're modifying are being reloaded in
     # dev mode, if they are in the BL plugin and haven't been copied to
-    # local, they won't be. But we do our best. 
-    config.to_prepare do
+    # local, they won't be. But we do our best.
+    config.to_prepare do    
       # Ordinary module over-ride to CatalogController
-      Blacklight::Catalog.send(:include,  
+      CatalogController.send(:include,  
           BlacklightAdvancedSearch::Controller  
       ) unless
-        Blacklight::Catalog.include?(   
+      CatalogController.include?(   
           BlacklightAdvancedSearch::Controller 
         )
         
