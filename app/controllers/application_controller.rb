b2e70f2edbbe1efda9491175753b0577f44c7551
Jonathan Rochkind <jonathan@dnil.net>
2011-09-14 13:37:33 -0400

    engine should not have had no-op application_controller, just asking for trouble

--- app/controllers/application_controller.rb
+++
@@ -1,5 +0,0 @@
-class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  
-end
