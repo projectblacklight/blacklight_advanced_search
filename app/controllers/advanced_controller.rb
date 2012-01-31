9edfd8730a5ee4ec770e312f0fcc6fb8ed13c39e
Chris Beer <chris_beer@wgbh.org>
2012-01-31 11:47:18 -0500

    explicitly copy the CatalogController config.

--- app/controllers/advanced_controller.rb
+++ app/controllers/advanced_controller.rb
@@ -1,5 +1,7 @@
 class AdvancedController < BlacklightAdvancedSearch::AdvancedController
 
+  copy_blacklight_config_from(CatalogController)
+
   blacklight_config.configure do |config|
     config.advanced_search.form_solr_parameters ||= {}
     config.advanced_search.url_key ||= 'advanced'
