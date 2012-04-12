7b00f5844d9fa01da1445e9fefb5e7d6bf3c14f0
Jonathan Rochkind <jonathan@dnil.net>
2012-04-12 17:09:15 -0400

    respect config include_in_advanced_search=false again, as documented

--- app/helpers/advanced_helper.rb
+++ app/helpers/advanced_helper.rb
@@ -33,8 +33,20 @@ module AdvancedHelper
     my_params
   end
 
-  def search_fields_for_advanced_search
-    blacklight_config.search_fields
+  def search_fields_for_advanced_search   
+    # If we could count on 1.9.3 with ordered hashes and
+    # Hash#select that worked reasonably, this would be trivial. 
+    # instead, a way compat with 1.8.7 and 1.9.x both. 
+    @search_fields_for_advanced_search ||= begin
+      # make it an ActiveSupport::OrderedHash if it needs to be
+      hash = blacklight_config.search_fields.class.new
+    
+      blacklight_config.search_fields.each_pair do |key, value|
+        hash[key] = value unless value.include_in_advanced_search == false
+      end
+      
+      hash
+    end
   end
   
 end
