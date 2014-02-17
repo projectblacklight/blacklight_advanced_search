8586ae6a1862a808a5d8ab7d7f24db8b48ca8827
Jonathan Rochkind <jonathan@dnil.net>
2014-02-17 18:14:38 -0500

    generator recognizes multiple asset file suffixes
    and prints more polite error message if it can't find
    the asset file at all. Closes #2.

--- lib/generators/blacklight_advanced_search/assets_generator.rb
+++ lib/generators/blacklight_advanced_search/assets_generator.rb
@@ -15,21 +15,38 @@ module BlacklightAdvancedSearch
   class AssetsGenerator < Rails::Generators::Base
     source_root File.join(BlacklightAdvancedSearch::Engine.root, 'app', 'assets')
 
-    def assets
-      original_css = File.binread("app/assets/stylesheets/application.css")
+    def css_asset
+      application_css_location = Dir["app/assets/stylesheets/application{.css,.scss,.css.scss}"].first
+
+      unless application_css_location
+        say_status "skipped", "Can not find an application.css, did not insert our require", :red
+        return
+      end
+
+      original_css = File.binread(application_css_location)
       if original_css.include?("require 'blacklight_advanced_search'")
         say_status("skipped", "insert into app/assets/stylesheets/application.css", :yellow)
       else        
-        insert_into_file "app/assets/stylesheets/application.css", :before => "*/" do
+        insert_into_file application_css_location, :before => "*/" do
           "\n *= require 'blacklight_advanced_search'\n\n"
         end
       end
+
+    end
+
+    def js_asset
+      application_js_location = Dir["app/assets/javascripts/application{.js,.coffee,.js.coffee}"].first
+
+      unless application_js_location
+        say_status "skipped", "Can not find an application.js, did not insert our require", :red
+        return
+      end
       
-      original_js = File.binread("app/assets/javascripts/application.js")
+      original_js = File.binread(application_js_location)
       if original_js.include?("require 'blacklight_advanced_search'")
         say_status("skipped", "insert into app/assets/javascripts/application.js", :yellow)
       else
-        insert_into_file "app/assets/javascripts/application.js", :after => %r{//= require ['"]?jquery['"]?$} do
+        insert_into_file application_js_location, :after => %r{//= require ['"]?jquery['"]?$} do
           "\n//= require 'blacklight_advanced_search'\n\n"
         end
       end
