3e23278678065277636d4a744f641b156c54a3d6
Jonathan Rochkind <jonathan@dnil.net>
2014-02-18 10:37:05 -0500

    remove ref to blacklight_config.default_qt
    Doesn't exist anymore. Thanks @jcoyne. See #10

--- app/controllers/blacklight_advanced_search/advanced_controller.rb
+++ app/controllers/blacklight_advanced_search/advanced_controller.rb
@@ -46,7 +46,7 @@ class BlacklightAdvancedSearch::AdvancedController < CatalogController
     input = HashWithIndifferentAccess.new
     input.merge!( search_context_params )
     
-    input.merge!( :qt => blacklight_config.advanced_search[:qt] || blacklight_config.default_qt || blacklight_config.default_solr_params[:qt], :per_page => 0)
+    input.merge!( :qt => blacklight_config.advanced_search[:qt] || blacklight_config.default_solr_params[:qt], :per_page => 0)
     input.merge!( blacklight_config.advanced_search[:form_solr_parameters] ) if blacklight_config.advanced_search[:form_solr_parameters]
     input[:q] ||= '{!lucene}*:*'
     
