84d8e20d51d3e604cd6c4fadffb6d2e0177bf204
Jonathan Rochkind <jonathan@dnil.net>
2014-03-04 10:38:08 -0500

    better comments

--- lib/blacklight_advanced_search/advanced_query_parser.rb
+++ lib/blacklight_advanced_search/advanced_query_parser.rb
@@ -1,34 +1,42 @@
 module BlacklightAdvancedSearch
+  # Can extract query elements from rails #params query params, and then parse
+  # them and convert them into a solr query with #to_solr
+  #
+  # #keyword_queries and #filters, which just return extracted elements of query
+  # params, may also be useful in display etc.
   class QueryParser
     include ParsingNestingParser # only one strategy currently supported. if BlacklightAdvancedSearch.config[:solr_type] == "parsing_nesting"
     include FilterParser
     attr_reader :config, :params
-    
+
     def initialize(params,config)
       @params = HashWithIndifferentAccess.new(params)
-      @config = config    
+      @config = config
     end
 
     def to_solr
       @to_solr ||= begin
         {
-          :q => process_query(params,config), 
-          :fq => generate_solr_fq() 
+          :q => process_query(params,config),
+          :fq => generate_solr_fq()
         }
       end
     end
-    
+
     # Returns "AND" or "OR", how #keyword_queries will be combined
     def keyword_op
       @params["op"] || "AND"
     end
-    # returns advanced-type keyword queries, see also keyword_op
+
+    # extracts advanced-type keyword query elements from query params,
+    # returns as a kash of field => query.
+    # see also keyword_op
     def keyword_queries
       unless(@keyword_queries)
         @keyword_queries = {}
 
         return @keyword_queries unless @params[:search_field] == ::AdvancedController.blacklight_config.advanced_search[:url_key]
-        
+
         config.search_fields.each do | key, field_def |
           if ! @params[ key.to_sym ].blank?
             @keyword_queries[ key ] = @params[ key.to_sym ]
@@ -37,7 +45,9 @@ module BlacklightAdvancedSearch
       end
       return @keyword_queries
     end
-    # returns just advanced-type filters
+
+    # extracts advanced-type filters from query params,
+    # returned as a hash of field => array of values
     def filters
       unless (@filters)
         @filters = {}
@@ -47,7 +57,7 @@ module BlacklightAdvancedSearch
             @filters[field] ||= []
             @filters[field] << value
           end
-        end        
+        end
       end
       return @filters
     end
@@ -55,6 +65,6 @@ module BlacklightAdvancedSearch
     def empty?
       filters.empty? && keyword_queries.empty?
     end
-    
+
   end
 end
