086ad65605cdc1040012b49c17b962fe5581628f
Jonathan Rochkind <jonathan@dnil.net>
2014-06-04 09:26:58 -0400

    avoid rspec deprecation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,20 @@ require 'capybara/rails'
 
 
 RSpec.configure do |config|
+  # Maintain this rspec2 behavior even in rspec3, until we
+  # adjust our stuff. Deprecation warning was:
+  # --------------------------------------------------------------------------------
+  # rspec-rails 3 will no longer automatically infer an example group's spec type
+  # from the file location. You can explicitly opt-in to this feature using this
+  # snippet:
 
+  # RSpec.configure do |config|
+  #   config.infer_spec_type_from_file_location!
+  # end
+
+  # If you wish to manually label spec types via metadata you can safely ignore
+  # this warning and continue upgrading to RSpec 3 without addressing it.
+  # --------------------------------------------------------------------------------
+  config.infer_spec_type_from_file_location!
 end
 
