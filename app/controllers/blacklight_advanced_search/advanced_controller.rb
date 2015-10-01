c12a24da167ab5e39dd5ccd5d1bf7c20018e5f2f
Jonathan Rochkind <jonathan@dnil.net>
2015-10-01 16:39:14 -0400

    Remove use of deprecated solr_search_params
    Using new BL 5.15 search_builder adjustment methods instead.
    In the process significantly simplifying some previously very messy
    code.

--- app/controllers/blacklight_advanced_search/advanced_controller.rb
+++ app/controllers/blacklight_advanced_search/advanced_controller.rb
@@ -1,8 +1,6 @@
 # Need to sub-class CatalogController so we get all other plugins behavior
 # for our own "inside a search context" lookup of facets. 
 class BlacklightAdvancedSearch::AdvancedController < CatalogController
-  include AdvancedHelper # so we get the #advanced_search_context method
-
 
   def index
     unless request.method==:post
@@ -12,48 +10,14 @@ class BlacklightAdvancedSearch::AdvancedController < CatalogController
 
   protected
   def get_advanced_search_facets
-    
-    search_context_params = {}
-    if (advanced_search_context.length > 0 )
-      # We have a search context, need to fetch facets from within
-      # that context -- but we dont' want to search within any
-      # existing :q or ADVANCED facets, so we remove those params.
-      adv_keys = blacklight_config.search_fields.keys.map {|k| k.to_sym}
-      trimmed_params = params.except *adv_keys
-      trimmed_params.delete(:f_inclusive) # adv facets
-      
-      search_context_params = solr_search_params(trimmed_params)
-
-      # Don't want to include the 'q' from basic search in our search
-      # context. Kind of hacky becuase solr_search_params insists on
-      # using controller.params, not letting us over-ride. 
-      search_context_params.delete(:q)
-      search_context_params.delete("q")
-      
-      # Also delete any facet-related params, or anything else
-      # we want to set ourselves
-      search_context_params.delete_if do |k, v|
-        k = k.to_s
-        (["facet.limit", "facet.sort", "f", "facets", "facet.fields", "per_page"].include?(k) ||                
-          k =~ /f\..+\.facet\.limit/ ||
-          k =~ /f\..+\.facet\.sort/
-        )        
-      end
+    # We want to find the facets available for the current search, but:
+    # * IGNORING current query (add in facets_for_advanced_search_form filter)
+    # * IGNORING current advanced search facets (remove add_advanced_search_to_solr filter)
+    response, _ = search_results(params, search_params_logic) do |search_builder|
+      #search_builder.except(:add_advanced_search_to_solr).append(:facets_for_advanced_search_form)
+      search_builder
     end
 
-    input = HashWithIndifferentAccess.new
-    input.merge!( search_context_params )
-
-    input[:per_page] = 0 # force
-
-    # force if set
-    input[:qt] = blacklight_config.advanced_search[:qt] if blacklight_config.advanced_search[:qt] 
-    
-    input.merge!( blacklight_config.advanced_search[:form_solr_parameters] ) if blacklight_config.advanced_search[:form_solr_parameters]
-
-    # ensure empty query is all records, to fetch available facets on entire corpus
-    input[:q] ||= '{!lucene}*:*'
-    
-    repository.search(input)
+    return response
   end
 end
--- lib/blacklight_advanced_search/advanced_search_builder.rb
+++ lib/blacklight_advanced_search/advanced_search_builder.rb
@@ -76,5 +76,25 @@ module BlacklightAdvancedSearch
         end
       end
     end
+
+    # A Solr param filter that is NOT included by default in the chain,
+    # but is appended by AdvancedController#index, to do a search
+    # for facets _ignoring_ the current query, we want the facets
+    # as if the current query weren't there. 
+    #
+    # Also adds any solr params set in blacklight_config.advanced_search[:form_solr_parameters]
+    def facets_for_advanced_search_form(solr_p)
+      # ensure empty query is all records, to fetch available facets on entire corpus
+      solr_p["q"]            = '{!lucene}*:*'
+
+      # We only care about facets, we don't need any rows. 
+      solr_p["rows"]         = "0"
+
+      # Anything set in config as a literal
+      if blacklight_config.advanced_search[:form_solr_parameters]
+        solr_p.merge!( blacklight_config.advanced_search[:form_solr_parameters] ) 
+      end
+
+    end
   end
 end
